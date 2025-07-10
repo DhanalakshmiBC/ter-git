96e372c832d876c1be9292b7477f504a6fdca6f1
Deepika <deepika@Deepikas-MacBook-Air.local>
2025-07-10 19:09:48 +0530

    chatgpt

--- vpc.tf
+++ vpc.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "main_subnet" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-1a"
   tags = {
     Name = "main-subnet"
   }
