7091514e3edae4ed0330c10079282227c95d5f5f
Deepika <deepika@Deepikas-MacBook-Air.local>
2025-07-10 18:26:16 +0530

    subnet

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "example_server1" {
     ami = "ami-05ffe3c48a9991133"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.public_subnets[0].id
+    associate_public_ip_address = true
     tags = {
         Name = "Depuka-server-1"
     }
