a59642bed0c2796a73d3ed585145fd643b3708bb
Deepika <deepika@Deepikas-MacBook-Air.local>
2025-07-10 19:38:22 +0530

    sad

--- main.tf
+++ main.tf
@@ -1,14 +1,82 @@
-# provider "aws" {
-#   region= "us-east-1"
-# }
+provider "aws" {
+  region = "us-west-1"
+}
 
-# resource "aws_instance" "example_server1" {
-#     ami = "ami-05ffe3c48a9991133"
-#     instance_type = "t2.micro"
-#     subnet_id              = aws_subnet.public_subnet_1.id
-#     key_name               = "my-key-pair"
-#     vpc_security_group_ids = [aws_security_group.terrafrom_sg.id]
-#     tags = {
-#         Name = "Terraform_Instance"
-#   }
-# }
+resource "aws_vpc" "own_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "own-vpc"
+  }
+  
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id = aws_vpc.own_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-1a"
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.own_vpc.id
+  tags = {
+    Name = "own-igw"
+  }
+}
+
+resource "aws_route_table" "main_route" {
+  vpc_id = aws_vpc.own_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "own-route-table"
+  }
+}
+
+resource "aws_route_table_association" "main_association" {
+  subnet_id = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.main_route.id
+}
+
+resource "aws_security_group" "own_sg" {
+  name = "allow-ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id = aws_vpc.own_vpc.id
+  
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  tags = {
+    Name = "allow-ssh"
+  }
+
+
+}
+
+resource "aws_instance" "own_inst" {
+  ami = "ami-061ad72bc140532fd"
+  security_groups = [aws_security_group.own_sg.id]
+  subnet_id = [aws_subnet.main_subnet.id]
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  tags = {
+    Name = "own-instance"
+  }  
+}
--- vpc.tf
+++ vpc.tf
@@ -1,90 +1,90 @@
-provider "aws" {
-  region = "us-west-1"
-}
+# provider "aws" {
+#   region = "us-west-1"
+# }
 
-# VPC
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main-vpc"
-  }
-}
+# # VPC
+# resource "aws_vpc" "main_vpc" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     Name = "main-vpc"
+#   }
+# }
 
-# Subnet
-resource "aws_subnet" "main_subnet" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-1a"
-  tags = {
-    Name = "main-subnet"
-  }
-}
+# # Subnet
+# resource "aws_subnet" "main_subnet" {
+#   vpc_id     = aws_vpc.main_vpc.id
+#   cidr_block = "10.0.1.0/24"
+#   availability_zone = "us-west-1a"
+#   tags = {
+#     Name = "main-subnet"
+#   }
+# }
 
-# Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main_vpc.id
-  tags = {
-    Name = "main-igw"
-  }
-}
+# # Internet Gateway
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.main_vpc.id
+#   tags = {
+#     Name = "main-igw"
+#   }
+# }
 
-# Route Table
-resource "aws_route_table" "main_rt" {
-  vpc_id = aws_vpc.main_vpc.id
+# # Route Table
+# resource "aws_route_table" "main_rt" {
+#   vpc_id = aws_vpc.main_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
 
-  tags = {
-    Name = "main-rt"
-  }
-}
+#   tags = {
+#     Name = "main-rt"
+#   }
+# }
 
-# Route Table Association
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main_subnet.id
-  route_table_id = aws_route_table.main_rt.id
-}
+# # Route Table Association
+# resource "aws_route_table_association" "a" {
+#   subnet_id      = aws_subnet.main_subnet.id
+#   route_table_id = aws_route_table.main_rt.id
+# }
 
-# Security Group
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main_vpc.id
+# # Security Group
+# resource "aws_security_group" "allow_ssh" {
+#   name        = "allow_ssh"
+#   description = "Allow SSH inbound traffic"
+#   vpc_id      = aws_vpc.main_vpc.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "allow_ssh"
-  }
-}
+#   tags = {
+#     Name = "allow_ssh"
+#   }
+# }
 
-# EC2 Instance
-resource "aws_instance" "web" {
-  ami           = "ami-061ad72bc140532fd" # Amazon Linux 2 AMI in us-east-1
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main_subnet.id
-  security_groups = [aws_security_group.allow_ssh.id]
+# # EC2 Instance
+# resource "aws_instance" "web" {
+#   ami           = "ami-061ad72bc140532fd" # Amazon Linux 2 AMI in us-east-1
+#   instance_type = "t2.micro"
+#   subnet_id     = aws_subnet.main_subnet.id
+#   security_groups = [aws_security_group.allow_ssh.id]
 
-  associate_public_ip_address = true
+#   associate_public_ip_address = true
 
-  tags = {
-    Name = "web-instance"
-  }
+#   tags = {
+#     Name = "web-instance"
+#   }
 
-   # Replace with your existing key pair name
-}
+#    # Replace with your existing key pair name
+# }
